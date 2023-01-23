22178ed7b1db464b580b3f1ed5e48a2e486d8903
Ashvin-Gupta <ashvingupta00@gmail.com>
2023-01-23 21:40:15 +0000

    Adding 2D code

---
+++ 2D/run_forward.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=4:mem=16gb:ngpus=1
+#PBS -l walltime=05:00:00
+#PBS -m ae
+#PBS -N 2D_model
+#PBS -o ./
+#PBS -e ./
+
+
+#Modules!
+module load anaconda3/personal cuda/11.4.2
+source activate PINNs_gpu
+
+nvidia-smi
+echo $CUDA_VISIBLE_DEVICES
+
+
+cd $PBS_O_WORKDIR
+echo $PBS_O_WORKDIR
+python main.py -d 2 -f "Spiral_diffGeom.mat" -m "/data/Different_geom" -p 
+
